30a3931c2e54e5bf94a10ed947e4dbee5b034b58
Younes Barrad <younesbarraddev@gmail.com>
2024-06-04 13:56:02 +0100

    test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,9 @@
-import vue from '@vitejs/plugin-vue'
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  hooks: {
-    'nitro:config': async (config) => {
-      config.rollupConfig = config.rollupConfig || {}
-      config.rollupConfig.plugins = config.rollupConfig.plugins || []
-      // @ts-expect-error
-      config.rollupConfig.plugins.push(vue())
-    },
+  modules: ['vue-email/nuxt'],
+  vueEmail: {
+    baseUrl: 'https://vue-email-demo.vercel.app/',
+    autoImport: true
   }
 })
