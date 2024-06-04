c93ebafb55f86b48026db0bc44c63e74340e2f6d
Younes Barrad <younesbarraddev@gmail.com>
2024-06-04 14:06:34 +0100

    test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['vue-email-edge/nuxt'],
+  modules: ['@vue-email/nuxt-edge'],
   vueEmail: {
     baseUrl: 'https://vue-email-demo.vercel.app/',
     autoImport: true
